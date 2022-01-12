83d5dd4d927bd794dc9628f25fe63417137a66f4
Olivier Henley <henley@adacore.com>
2022-01-12 14:28:07 -0500

    implement Pgprot, clean up kernel.adb

--- flash_led/src/kernel.adb
+++ flash_led/src/kernel.adb
@@ -2,6 +2,10 @@ With Ada.Unchecked_Conversion;
 With System.Machine_Code;
 Package Body Kernel Is
 
+   Type Bool Is (NO, YES);
+   For Bool Use (NO => 0, YES => 1);
+   For Bool'Size Use 1;
+
    Procedure Printk_C (S : String) With
      Import        => True,
      Convention    => C,
@@ -10,7 +14,7 @@ Package Body Kernel Is
    Procedure Printk (S : String) Is
    Begin
       Printk_C (S & Ascii.Lf & Ascii.Nul);
-   End Printk;
+   End;
 
    Procedure Print_Access_Hex_C (A : System.Address) With
      Import        => True,
@@ -31,8 +35,6 @@ Package Body Kernel Is
    Begin
       Print_Access_Hex_C (A);
    End;
-
-   -- Gpio
 
    Type Gpio_Desc_Acc Is New System.Address;
 
@@ -61,7 +63,7 @@ Package Body Kernel Is
    Function Gpio_Request (Gpio : Ic.Unsigned; Label : String) Return Ic.Int Is
    Begin
       Return Gpio_Request_C (Gpio, Label & Ascii.Lf & Ascii.Nul);
-   End Gpio_Request;
+   End;
 
    Function Gpio_Direction_Output
      (Gpio : Ic.Unsigned; Value : Ic.Int) Return Ic.Int
@@ -69,13 +71,13 @@ Package Body Kernel Is
       Desc : Gpio_Desc_Acc := Gpio_To_Desc_C (Gpio);
    Begin
       Return Gpiod_Direction_Output_Raw_C (Desc, Value);
-   End Gpio_Direction_Output;
+   End;
 
    Function Gpio_Get_Value (Gpio : Ic.Unsigned) Return Ic.Int Is
       Desc : Gpio_Desc_Acc := Gpio_To_Desc_C (Gpio);
    Begin
       Return Gpiod_Get_Raw_Value_C (Desc);
-   End Gpio_Get_Value;
+   End;
 
    Procedure Gpio_Free_C (Gpio : Ic.Unsigned) With
      Import        => True,
@@ -85,7 +87,7 @@ Package Body Kernel Is
    Procedure Gpio_Free (Gpio : Ic.Unsigned) Is
    Begin
       Gpio_Free_C (Gpio);
-   End Gpio_Free;
+   End;
 
    Function Msecs_To_Jiffies_C (M : Ic.Unsigned) Return Ic.Unsigned_Long With
      Import        => True,
@@ -95,7 +97,7 @@ Package Body Kernel Is
    Function Msecs_To_Jiffies (M : Ic.Unsigned) Return Ic.Unsigned_Long Is
    Begin
       Return Msecs_To_Jiffies_C (M);
-   End Msecs_To_Jiffies;
+   End;
 
    Type Pgprot_T Is Mod 2**64;
    For Pgprot_T'Size Use 64;
@@ -104,98 +106,53 @@ Package Body Kernel Is
      Convention    => C,
      External_Name => "__ioremap";
 
-   --Type Bool Is (F, T);
-   --For Bool Use (False => 0, True => 1);
-   --For Bool'Size Use 1;
+   Function Ioremap (Phys_Addr : Phys_Addr_T; Size : Ic.Size_T) Return Iomem_Access Is
+        Type Memory_T Is Range 0 .. 5;
 
-   --Function Ioremap (Phys_Addr : Phys_Addr_T; Size : Ic.Size_T) Return Iomem_Access Is
-   --    Type PTE_Flags Is Record
-   --
-   --    End Record;
-   --Begin
-   --End;
-   --Ioremap_C (Phys_Addr, )
+        MT_DEVICE_NGnRnE : Memory_T := 0;
+        MT_DEVICE_NGnRE  : Memory_T := 1;
+        MT_DEVICE_GRE    : Memory_T := 2;
+        MT_NORMAL_NC     : Memory_T := 3;
+        MT_NORMAL        : Memory_T := 4;
+        MT_NORMAL_WT     : Memory_T := 5;
 
-   --Type Bool Is (False, True);
-   --For Bool Use (False => 0, True => 1);
+        Function PTE_ATTRINDX (Mt : Memory_T) Return Pgprot_T Is
+        Begin
+            Return Pgprot_T(Mt * 2#1#e+2);
+        End;
 
-   --For Bool'Size Use 1;
+        -- Page Table Entry
+        PTE_VALID      : Pgprot_T := 2#1#e+0;
+        PTE_WRITE      : Pgprot_T := 2#1#e+51;
+        PTE_DIRTY      : Pgprot_T := 2#1#e+55;
+        PTE_SPECIAL    : Pgprot_T := 2#1#e+56;
+        PTE_PROT_NONE  : Pgprot_T := 2#1#e+58;
+        PTE_TYPE_MASK  : Pgprot_T := 3 * 2#1#e+0;
+        PTE_TYPE_FAULT : Pgprot_T := 2#0#e+0;
+        PTE_TYPE_PAGE  : Pgprot_T := 3 * 2#1#e+0;
+        PTE_TABLE_BIT  : Pgprot_T := 2#1#e+1;
+        PTE_USER       : Pgprot_T := 2#1#e+6;
+        PTE_RDONLY     : Pgprot_T := 2#1#e+7;
+        PTE_SHARED     : Pgprot_T := 3 * 2#1#e+8;  -- Inner Shareable
+        PTE_AF         : Pgprot_T := 2#1#e+10;     -- Access Flag
+        PTE_NG         : Pgprot_T := 2#1#e+11;     -- NG
+        PTE_DBM        : Pgprot_T := 2#1#e+51;     -- Dirty Bit Management
+        PTE_CONT       : Pgprot_T := 2#1#e+52;     -- Contiguous Range
+        PTE_PXN        : Pgprot_T := 2#1#e+53;     -- Privileged XN
+        PTE_UXN        : Pgprot_T := 2#1#e+54;     -- User XN
+        PTE_HYP_XN     : Pgprot_T := 2#1#e+54;     -- HYP XN
 
-   Function Ioremap
-     (Phys_Addr : Phys_Addr_T; Size : Ic.Size_T) Return Iomem_Access
-   Is
-
-      -- Arm64/Include/Asm/Pgtable-Prot.H
-      -- #define PROT_DEVICE_NGnRE (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_DIRTY | PTE_WRITE | PTE_ATTRINDX(MT_DEVICE_NGnRE))
-      -- #define PROT_DEFAULT              (_PROT_DEFAULT | PTE_MAYBE_NG)
-      -- #define PTE_MAYBE_NG              (Arm64_Kernel_Unmapped_At_El0() ? PTE_NG : 0)
-      -- #define _PROT_DEFAULT             (PTE_TYPE_PAGE | PTE_AF | PTE_SHARED)
-
-      -- Arm64/Include/Asm/Pgtable-Hwdef.H
-      -- /*
-      -- * Level 3 Descriptor (PTE).
-      -- */
-      -- #define PTE_TYPE_MASK             (_AT(Pteval_T, 3) << 0)
-      -- #define PTE_TYPE_FAULT            (_AT(Pteval_T, 0) << 0)
-      -- #define PTE_TYPE_PAGE             (_AT(Pteval_T, 3) << 0)
-      -- #define PTE_TABLE_BIT             (_AT(Pteval_T, 1) << 1)
-      -- #define PTE_USER                  (_AT(Pteval_T, 1) << 6)         /* AP[1] */
-      -- #define PTE_RDONLY                (_AT(Pteval_T, 1) << 7)         /* AP[2] */
-      -- #define PTE_SHARED                (_AT(Pteval_T, 3) << 8)         /* SH[1:0], Inner Shareable */
-      -- #define PTE_AF                    (_AT(Pteval_T, 1) << 10)        /* Access Flag */
-      -- #define PTE_NG                    (_AT(Pteval_T, 1) << 11)        /* NG */
-      -- #define PTE_DBM                   (_AT(Pteval_T, 1) << 51)        /* Dirty Bit Management */
-      -- #define PTE_CONT                  (_AT(Pteval_T, 1) << 52)        /* Contiguous Range */
-      -- #define PTE_PXN                   (_AT(Pteval_T, 1) << 53)        /* Privileged XN */
-      -- #define PTE_UXN                   (_AT(Pteval_T, 1) << 54)        /* User XN */
-      -- #define PTE_HYP_XN                (_AT(Pteval_T, 1) << 54)        /* HYP XN */
-
-      -- Arm64/Include/Asm/Pgtable-Prot.H
-      -- /*
-      -- * Software Defined PTE Bits Definition.
-      -- */
-      -- #define PTE_VALID             (_AT(Pteval_T, 1) << 0)
-      -- #define PTE_WRITE             (PTE_DBM)            /* Same As DBM (51) */
-      -- #define PTE_DIRTY             (_AT(Pteval_T, 1) << 55)
-      -- #define PTE_SPECIAL               (_AT(Pteval_T, 1) << 56)
-      -- #define PTE_PROT_NONE             (_AT(Pteval_T, 1) << 58) /* Only When !PTE_VALID */
-
-      -- Arm64/Include/Asm/Pgtable-Hwdef.H
-      -- /*
-      -- * AttrIndx[2:0] Encoding (Mapping Attributes Defined In The MAIR* Registers).
-      -- */
-      -- #define PTE_ATTRINDX(T)   (_AT(Pteval_T, (T)) << 2)
-      -- #define PTE_ATTRINDX_MASK (_AT(Pteval_T, 7) << 2)
-      --
-
-      --/*
-      -- * Memory Types Available.
-      -- */
-      -- Arm64/Include/Asm/Memory.H
-      -- #define MT_DEVICE_NGnRnE  0
-      -- #define MT_DEVICE_NGnRE   1
-      -- #define MT_DEVICE_GRE             2
-      -- #define MT_NORMAL_NC              3
-      -- #define MT_NORMAL             4
-      -- #define MT_NORMAL_WT              5
-
-      --__Ioremap((Phys_Addr + Offset), (4), ((Pgprot_T) {
-      --(((((((Pteval_T)(3)) << 0) | -- PTE_TYPE_MASK
-      --(((Pteval_T)(1)) << 10) | -- PTE_AF
-      --(((Pteval_T)(3)) << 8)) | -- PTE_SHARED
-      --(Arm64_Kernel_Unmapped_At_El0() ? (((Pteval_T)(1)) << 11) : 0)) | -- If Arm64_Kernel_Unmapped_At_El0() Then PTE_NG Else 0
-      --(((Pteval_T)(1)) << 53) | -- PTE_PXN
-      --(((Pteval_T)(1)) << 54) | -- PTE_UXN
-
-      --(((Pteval_T)(1)) << 55) | -- PTE_DIRTY
-      --((((Pteval_T)(1)) << 51)) | -- PTE_DBM
-
-      --(((Pteval_T)((1))) << 2))) } )); -- PTE_ATTRINDX(MT_DEVICE_NGnRE)
-
-      Pgprot : Pgprot_T := 16#00e8_0000_0000_0707#;
+        Pgprot : Pgprot_T := (PTE_TYPE_MASK Or 
+                              PTE_AF Or
+                              PTE_SHARED Or
+                              PTE_PXN Or
+                              PTE_UXN Or
+                              PTE_DIRTY Or
+                              PTE_DBM Or
+                              PTE_ATTRINDX (MT_DEVICE_NGnRE));
    Begin
       Return Ioremap_C (Phys_Addr, Size, Pgprot);
-   End Ioremap;
+   End;
 
    Procedure Queue_Delayed_Work_On_C
      (Cpu    : Ic.Int; Wq : Workqueue_Struct_Access; Work : Delayed_Work_Access;
@@ -210,7 +167,7 @@ Package Body Kernel Is
    Is
    Begin
       Queue_Delayed_Work_On_C (1, Wq, Work, Delayy);
-   End Queue_Delayed_Work;
+   End;
 
    Function Alloc_Workqueue_Key_C
      (Format         : String; Flags : Ic.Unsigned; Max_Active : Ic.Int;
@@ -221,19 +178,18 @@ Package Body Kernel Is
      External_Name => "__alloc_workqueue_key";
 
    Function Alloc_Workqueue (Name : String) Return Workqueue_Struct_Access Is
-      Type Bit Is Mod 2**1;
       Type Workqueue_Flags Is Record
-         WQ_UNBOUND          : Bit;
-         WQ_FREEZABLE        : Bit;
-         WQ_MEM_RECLAIM      : Bit;
-         WQ_HIGHPRI          : Bit;
-         WQ_CPU_INTENSIVE    : Bit;
-         WQ_SYSFS            : Bit;
-         WQ_POWER_EFFICIENT  : Bit;
-         WQ_DRAINING         : Bit;
-         WQ_ORDERED          : Bit;
-         WQ_LEGACY           : Bit;
-         WQ_ORDERED_EXPLICIT : Bit;
+         WQ_UNBOUND          : Bool;
+         WQ_FREEZABLE        : Bool;
+         WQ_MEM_RECLAIM      : Bool;
+         WQ_HIGHPRI          : Bool;
+         WQ_CPU_INTENSIVE    : Bool;
+         WQ_SYSFS            : Bool;
+         WQ_POWER_EFFICIENT  : Bool;
+         WQ_DRAINING         : Bool;
+         WQ_ORDERED          : Bool;
+         WQ_LEGACY           : Bool;
+         WQ_ORDERED_EXPLICIT : Bool;
       End Record;
       For Workqueue_Flags'Size Use 32;
       For Workqueue_Flags Use Record
@@ -250,8 +206,12 @@ Package Body Kernel Is
          WQ_ORDERED_EXPLICIT At 0 Range 19 .. 19;
       End Record;
       Flags : Workqueue_Flags :=
-        (WQ_UNBOUND => 1, WQ_ORDERED => 1, WQ_ORDERED_EXPLICIT => 1,
-         WQ_LEGACY  => 1, WQ_MEM_RECLAIM => 1, Others => 0);
+        (WQ_UNBOUND => YES, 
+         WQ_ORDERED => YES, 
+         WQ_ORDERED_EXPLICIT => YES,
+         WQ_LEGACY  => YES, 
+         WQ_MEM_RECLAIM => YES, 
+         Others => NO);
       Wq_Flags : Ic.Unsigned;
       For Wq_Flags'Address Use Flags'Address;
    Begin
@@ -259,7 +219,7 @@ Package Body Kernel Is
         Alloc_Workqueue_Key_C
           ("%s", Wq_Flags, 1, System.Null_Address, System.Null_Address,
            Name);
-   End Alloc_Workqueue;
+   End;
 
    Procedure Delayed_Work_Timer_Fn_C (Data : Ic.Unsigned_Long) With
      Import        => True,
@@ -269,7 +229,7 @@ Package Body Kernel Is
    Procedure Delayed_Work_Timer_Fn (Data : Ic.Unsigned_Long) Is
    Begin
       Delayed_Work_Timer_Fn_C (Data);
-   End Delayed_Work_Timer_Fn;
+   End;
 
    Procedure Cancel_Delayed_Work_C (Delayed_Work : Delayed_Work_Access) With
      Import        => True,
@@ -279,7 +239,7 @@ Package Body Kernel Is
    Procedure Cancel_Delayed_Work (Delayed_Work : Delayed_Work_Access) Is
    Begin
       Cancel_Delayed_Work_C (Delayed_Work);
-   End Cancel_Delayed_Work;
+   End;
 
    Procedure Flush_Workqueue_C (Wq : Workqueue_Struct_Access) With
      Import        => True,
@@ -289,7 +249,7 @@ Package Body Kernel Is
    Procedure Flush_Workqueue (Wq : Workqueue_Struct_Access) Is
    Begin
       Flush_Workqueue_C (Wq);
-   End Flush_Workqueue;
+   End;
 
    Procedure Destroy_Workqueue_C (Wq : Workqueue_Struct_Access) With
      Import        => True,
@@ -299,7 +259,7 @@ Package Body Kernel Is
    Procedure Destroy_Workqueue (Wq : Workqueue_Struct_Access) Is
    Begin
       Destroy_Workqueue_C (Wq);
-   End Destroy_Workqueue;
+   End;
 
    Procedure Io_Write_32 (Val : U32; Addr : Iomem_Access) Is
       Use System.Machine_Code;
@@ -307,11 +267,9 @@ Package Body Kernel Is
       Asm (Template => "dsb st", Clobber => "memory", Volatile => True);
 
       Asm (Template => "str %w0, [%1]",
-           Inputs    =>
-             (U32'Asm_Input ("rZ", Val),
-              Iomem_Access'Asm_Input ("r", Addr)),
+           Inputs    => (U32'Asm_Input ("rZ", Val), Iomem_Access'Asm_Input ("r", Addr)),
            Volatile => True);
-   End Io_Write_32;
+   End;
 
    Function Io_Read_32 (Addr : Iomem_Access) Return U32 Is
       Use System.Machine_Code;
@@ -321,8 +279,7 @@ Package Body Kernel Is
            Inputs    => (Iomem_Access'Asm_Input ("r", Addr)),
            Outputs   => (Kernel.U32'Asm_Output ("=r", Value)),
            Volatile  => True);
-
       Return Value;
-   End Io_Read_32;
+   End;
 
 End Kernel;
