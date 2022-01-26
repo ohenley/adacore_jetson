18c3124fd327677e7d9a691ed6b1470185af834e
Olivier Henley <henley@adacore.com>
2022-01-26 10:26:32 -0500

    tidy up main.c

--- flash_led/src/main.c
+++ flash_led/src/main.c
@@ -7,7 +7,6 @@
 
 extern void ada_init_module (void);
 extern void ada_cleanup_module (void);
-extern void print_access_hex (u64*);
 
 int init_module(void)
 {
@@ -20,7 +19,7 @@ void cleanup_module(void)
     ada_cleanup_module();
 }
 
-void print_access_hex(u64* value)
+extern void print_access_hex(u64* value)
 {
     printk("%p\n", value);
 }
